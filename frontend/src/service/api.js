26c1cc4e87b472df63297f42e5caebe05d1ca059
saipranav333444 <109069619+saipranav333444@users.noreply.github.com>
2025-12-02 00:34:05 +0530

    Change API base URL to production server

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://clothing-ecommerce-flax.vercel.app/",
   withCredentials: true,
 });
